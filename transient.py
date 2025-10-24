78329d313611bc613e408256a4ac0b0eed8e645a
bullet-with-butterfly-wings <dej.jonas12@gmail.com>
2025-10-24 12:07:07 +0100

    Some pics

--- transient.py
+++ transient.py
@@ -7,14 +7,22 @@ import os
 
 
 plotting = False
-path = Path("rabi3/oscillations_pretty")
+path = Path("rabi3/detuning")  # Change to desired path
+
 
 
 def rabi_decay(t, A, B, C, D, E):
     return A * np.exp(-B * t) * np.cos(C * t + D) + E
 # Select only rows where time is between -0.1 and 3.7
 
-voltage_range = sorted(list(filter(lambda x: "." not in x, os.listdir(path))), key=lambda x: int(x))
+scanning_range = []
+is_detuning = path.name == "detuning"
+if is_detuning:
+    scanning_range = sorted(list(filter(lambda x: "." not in x, os.listdir(path))), key=lambda x: int(x[0])*(-1 if "m" in x else 1))
+else:
+    scanning_range = sorted(list(filter(lambda x: "." not in x, os.listdir(path))), key=lambda x: int(x))
+
+scanning_range = scanning_range[:7]  # Use all voltages
 frequencies = []
 lifetimes = []
 amplitude = []
@@ -23,15 +31,16 @@ frequencies_error = []
 amplitude_error = []
 
 
-for voltage in voltage_range:
-    files = [f.name for f in path.glob(f"{voltage}/*.csv")]  # Sort by file name
+
+for parameter in scanning_range:
+    files = [f.name for f in path.glob(f"{parameter}/*.csv")]  # Sort by file name
     frequencies_sample = []
     lifetime_sample = [] #data from this voltage
     amplitude_sample = []
     total = None
 
     for wavefront in files:
-        df = pd.read_csv(path/ voltage / wavefront, skiprows=1).to_numpy()
+        df = pd.read_csv(path/ parameter / wavefront, skiprows=1).to_numpy()
         mask = (df[:, 2] > 0) # only oscillations
         df = df[mask]
         #df = df[:1000,:]
@@ -65,36 +74,46 @@ for voltage in voltage_range:
     amplitude_error.append(np.std(amplitude_sample))
 
 buffer = []
-for v in voltage_range:
-    if int(v) % 5 != 0:
-        buffer.append(int(v)+0.5)
-    else:
-        buffer.append(int(v))
-voltage_range = buffer 
-plt.plot(voltage_range, frequencies, marker="o")
-plt.errorbar(voltage_range, frequencies, yerr=frequencies_error)
+if is_detuning:
+    for v in scanning_range:
+        if "m" in v:
+            buffer.append(int(v[0])*-1)
+        else:
+            buffer.append(int(v[0]))
+else:
+    for v in scanning_range:
+        if int(v) % 5 != 0:
+            buffer.append(int(v)+0.5)
+        else:
+            buffer.append(int(v))
+        
+scanning_range = buffer
+plt.plot(scanning_range, frequencies, marker="o")
+plt.errorbar(scanning_range, frequencies, yerr=frequencies_error)
 #fit straight line
-coeffs = np.polyfit(voltage_range, frequencies, 1)
-fit_line = np.poly1d(coeffs)
-plt.plot(voltage_range, fit_line(voltage_range), linestyle='--', label=f"{coeffs[0]:.2e}*V + {coeffs[1]:.2e}")
-plt.xlabel("Voltage [V]")
+if not is_detuning:
+    coeffs = np.polyfit(scanning_range, frequencies, 1)
+    fit_line = np.poly1d(coeffs)
+    plt.plot(scanning_range, fit_line(scanning_range), linestyle='--', label=f"{coeffs[0]:.2e}*V + {coeffs[1]:.2e}")
+xlabel = "Voltage [V]" if not is_detuning else "Detuning [kHz]"
+plt.xlabel(xlabel)
 plt.ylabel("Rabi Frequency [kHz]")
-plt.title("Rabi Frequency vs Voltage")
+plt.title(f"Rabi Frequency vs {'Detuning' if is_detuning else 'Voltage'}")
 plt.legend()
 plt.show()
 
-plt.plot(voltage_range, lifetimes, marker="o")
-plt.errorbar(voltage_range, lifetimes, yerr=lifetimes_error)
-plt.xlabel("Voltage [V]")
+plt.plot(scanning_range, lifetimes, marker="o")
+plt.errorbar(scanning_range, lifetimes, yerr=lifetimes_error)
+plt.xlabel(xlabel)
 plt.ylabel("Lifetime [ms]")
-plt.title("Rabi Lifetime vs Voltage")
+plt.title(f"Rabi Lifetime vs {'Detuning' if is_detuning else 'Voltage'}")
 plt.show()
 
-plt.plot(voltage_range, amplitude, marker="o")
-plt.errorbar(voltage_range, amplitude, yerr=amplitude_error)
-plt.xlabel("Voltage [V]")
+plt.plot(scanning_range, amplitude, marker="o")
+plt.errorbar(scanning_range, amplitude, yerr=amplitude_error)
+plt.xlabel(xlabel)
 plt.ylabel("Amplitude [V]")
-plt.title("Rabi Amplitude vs Voltage")
+plt.title(f"Rabi Amplitude vs {'Detuning' if is_detuning else 'Voltage'}")
 plt.show()
 
 """
